415bb1ae61c3846597ce5e744c555481e40162e0
bspark05 <bpark21@asu.edu>
2014-12-01 12:51:56 -0700

    fourth

--- Database/Neo4j/Neo4jRESTAPI.py
+++ Database/Neo4j/Neo4jRESTAPI.py
@@ -3,3 +3,16 @@ Created on Dec 1, 2014
 
 @author: Administrator
 '''
+
+import Database.Neo4j.Relationship as relationship
+import httplib
+
+SERVER_ROOT_URI = 'http://localhost:7474'
+
+def getServerStatus(self):
+    status = 500
+    try:
+        url = SERVER_ROOT_URI
+        
+        
+        
