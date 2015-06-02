e6f044bf46a2abac765cf0734f781cc0df7e07e5
bspark05 <bpark21@asu.edu>
2015-06-02 16:49:22 -0700

    06022015-1

--- Web/APIs/Geocoding.py
+++ Web/APIs/Geocoding.py
@@ -3,16 +3,33 @@ Created on Jun 1, 2015
 
 @author: Bumsub
 '''
-from geopy.geocoders import Nominatim
+#from geopy.geocoders import Nominatim
+import geocoder
 
 def geocodeList(addressExcelList):
-    geolocator = Nominatim()
-    searchingAddress = ''
-    for rows in addressExcelList:
-        searchingAddress+=addressExcelList[rows][3].value
-        searchingAddress+=addressExcelList[rows][4].value
-        searchingAddress+=addressExcelList[rows][6].value
-        searchingAddress+=addressExcelList[rows][7].value    
-        
-        #location = geolocator.geocode(addressExcelList[rows][columns])
     
+    #geolocator = Nominatim()
+    searchingAddress = ''
+    resultList = []
+    count = 0
+    for rows in addressExcelList:
+        searchingAddress=''
+        searchingAddress+=str(rows[3].value)
+        searchingAddress+=' '
+        searchingAddress+=str(rows[4].value)
+        searchingAddress+=' '
+        searchingAddress+=str(rows[6].value)
+        searchingAddress+=' '
+        searchingAddress+=str(rows[7].value)    
+        
+        print(searchingAddress)
+        location = geocoder.google(searchingAddress)
+        print(location.latlng)
+        if location.latlng == [] :
+            resultList.append(['',''])
+        else:
+            resultList.append(location.latlng)
+            
+        print(count)
+        count+=1
+    return resultList
